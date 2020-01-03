61b1e9ed5ea8e1e31f753579f246badc29a4a83c
jackyfive <1833792912@qq.com>
2020-01-03 09:01:08 +0800

    添加登录错误拦截

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,7 +3,6 @@ import {Message} from 'element-ui'
 export default ({$axios}) => {
   
   $axios.onError(err=>{
-
       if(err.response.data.statusCode===400){
         let message=err.response.data.message
           Message.warning({message});
