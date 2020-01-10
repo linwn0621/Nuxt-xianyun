9816c52880fd77300da7f8626323b6b18de7a758
jackyfive <1833792912@qq.com>
2020-01-10 17:08:29 +0800

    旅游攻略页侧边导航栏完成

--- components/post/cblnav.vue
+++ components/post/cblnav.vue
@@ -1,56 +1,140 @@
 <template>
-  <div class="menus">
-    <ul>
-<li :class="[menu,{active:aa===index}]"  v-for="(item,index) in form" :key="index"  @mouseenter="enter(index)" @mouseleave="leave">
-  {{item.type}}
-</li>
-    </ul>
-
+  <div class="menus" @mouseleave="leave">
+    <div class="box">
+      <!-- 侧边导航栏 -->
+      <div
+       
+        :class="[menu,{active:current===index}]"
+        v-for="(item,index) in form"
+        :key="index"
+        @mouseenter="enter(item,index)"
+        
+      >{{item.type}}
+      </div>
+    </div>
+    <!-- 弹出框 -->
+    <div class="sub-cat" v-if="isshow">
+      <div v-for="(item,index) in citys.children" :key="index">
+        <a href="#">
+          <span class="num">{{index+1}}</span>
+            <strong>{{item.city}}</strong>
+        <span class="desc">{{item.desc}}</span>
+        </a>
+      
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  
-data () {
-  return {
-    form:[],
-    aa:null,
-    menu: "menu"
-  }
-},
-
-mounted () {
-  this.$axios({
-    url:"/posts/cities"
-  }).then(res=>{
-    console.log(1,res)
-    this.form=res.data.data
-  })
-},
-methods: {
-  enter(index){
-console.log(index)
-this.aa=index
+  data() {
+    return {
+      current: -1,
+      menu: "menu",
+      form: [], //导航菜单名
+      citys: [],//导航菜单项内容
+      isshow: false //弹出框显示隐藏
+    };
   },
-  leave(){
-    this.aa=null
+
+  mounted() {
+    // 请求城市数据
+    this.$axios({
+      url: "/posts/cities"
+    }).then(res => {
+      console.log(1, res);
+      this.form = res.data.data;
+    });
+  },
+  methods: {
+    // 鼠标移出
+    enter(item, index) {
+      this.citys = item;
+      this.current = index;
+      this.isshow = true;
+    },
+    // 鼠标移出
+    leave() {
+      this.current = -1;
+      this.isshow = false;
+    }
+
+  
   }
-}
-}
+};
 </script>
 
 <style lang="less" scoped>
-.menu{
-  line-height: 40px;
+.box {
   border: 1px solid #ddd;
-  border-bottom-width: 0px;
-  border-right: 1px solid #ddd;
+  border-bottom: none;
+  border-right: none;
+  width: 260px;
+ .menu {
+  line-height: 40px;
   padding: 0 20px;
   font-size: 14px;
+   border-bottom: 1px solid #ddd;
+  border-right: 1px solid #ddd;
+  position: relative;
+  z-index: 3;
+  &::after{
+    content: "";
+    display: block;
+    width: 10px;
+    height: 10px;
+    border-top:1px solid #ddd;
+    border-right:1px solid #ddd;
+    position: absolute;
+       right: 20px;
+    top: 15px;
+    transform:rotate(45deg);
 }
-.active{
+}
+
+}
+.sub-cat {
+  width: 350px;
+  position: absolute;
+  padding: 10px 20px;
+  left: 260px;
+  top: 0;
+  background: white;
+  box-sizing: border-box;
+  border: 1px solid #ddd;
+  z-index: 2;
   color: #ffa500;
-  border-right-color:white;
+   font-size: 14px;
+  .num{
+font-size: 24px;
+font-style: oblique;
+  }
+  strong{
+   margin: 0 10px;
+  &:hover{
+   
+   text-decoration:underline
+  }
+  }
+
+.desc{
+  color: #999;
+   &:hover{
+   
+   text-decoration:underline
+  }
 }
+
+}
+.active {
+  color: #ffa500;
+  border-right-color: white !important;
+  &::after{
+  content: "";
+  border-top-color: #ffa500  !important;
+   border-right-color:#ffa500  !important;
+}
+}
+
 </style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="container">
-    <el-row type="flex" class="row-bg" justify="space-between">
-      <el-col :span="6">
-        <div class="">
+    <el-row type="flex" class="row-bg" justify="space-between" >
+      <el-col :span="6" >
+        <div class>
           <cblnav></cblnav>
         </div>
         <div class="tjcs">
           <h4>推荐城市</h4>
-          <a href="#"><img src="../../images/pic_sea.jpeg" alt=""></a>
-          </div>
+          <a href="#">
+            <img src="../../images/pic_sea.jpeg" alt />
+          </a>
+        </div>
       </el-col>
       <el-col :span="17">
         <el-row>
-          <div class="">2</div>
+          <div class>2</div>
         </el-row>
         <el-row>
-          <div class="">2</div>
+          <div class>2</div>
         </el-row>
       </el-col>
     </el-row>
@@ -43,22 +45,16 @@ export default {
   padding: 20px 0;
 }
 
-.el-header {
-  background-color: blueviolet;
-}
-.el-main {
-  background-color: blue;
-}
-.tjcs{
-         margin-top: 20px;
-  h4{
+
+.tjcs {
+  margin-top: 20px;
+  h4 {
     font-weight: 400;
-     padding-bottom: 10px;
+    padding-bottom: 10px;
     border-bottom: 1px solid #ddd;
     margin-bottom: 10px;
-  
   }
-  img{
+  img {
     width: 100%;
   }
 }
